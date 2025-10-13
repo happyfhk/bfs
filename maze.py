6b2adb275b7f794265ce03e06850355997077c4e
lsfp45 <grassooooolut@gmail.com>
2025-10-13 17:31:41 +0900

    Update maze.py

--- maze.py
+++ maze.py
@@ -1,10 +1,17 @@
 #pip install Pygame
 
 import pygame
+from collections import deque
+import time
 
 pygame.init()
+width=800
+height=600
+screen=pygame.display.set_mode((width,height))
+pygame.display.set_caption("미로 찾기")
 
-winWidth=600
-winHeight=450
-screen=pygame.display.set_mode((winWidth,winHeight))
-
+while True:
+    num=int(input("미로의 크기를 입력하세요"))
+    
+    if(num<1):
+        break
